06d11161d4fb26158f442da5489ee274f22d1191
Choque <diego.choque.severiche@gmail.com>
2024-08-09 03:41:31 -0400

    fix(zip-code): Replace postal code by zip code

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,4 @@
 use App\Http\Controllers\SquareMeterController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/price-m2/zip-codes/{postal_code}/aggregate/{type}', [SquareMeterController::class, 'index']);
+Route::get('/price-m2/zip-codes/{zip_code}/aggregate/{type}', [SquareMeterController::class, 'index']);
