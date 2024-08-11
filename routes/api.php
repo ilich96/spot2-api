72504e18c78ea7c5092ae2272df1112239eb6219
Choque <diego.choque.severiche@gmail.com>
2024-08-11 16:41:45 -0400

    feat(testing): Add controller to test

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\SquareMeterController;
+use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/price-m2/zip-codes/{zip_code}/aggregate/{type}', [SquareMeterController::class, 'index']);
+Route::get('/testing', [TestController::class, 'index']);
