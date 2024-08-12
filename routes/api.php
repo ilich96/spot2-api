85f278bd8442d403f49743ce52908403c6487222
Choque <diego.choque.severiche@gmail.com>
2024-08-12 15:05:30 -0400

    feat(test): Update api route file

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,6 @@
 <?php
 
 use App\Http\Controllers\SquareMeterController;
-use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/price-m2/zip-codes/{zip_code}/aggregate/{type}', [SquareMeterController::class, 'index']);
-Route::get('/testing', [TestController::class, 'index']);
